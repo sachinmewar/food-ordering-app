47d9ad74a231eed158a885a365cb12ee8ecfdcdd
unknown <sachinmewar800@gmail.com>
2021-05-26 21:36:20 +0530

    Added Image.js component which will display Image

---
+++ src/components/Image.js
@@ -0,0 +1,11 @@
+import './Image.css';
+
+const Image = () => {
+   return (
+      <div className='image-container'>
+         <img src='/foodImage.jpg' alt='Loading Failed' height='200px' width='200px' />
+      </div>
+   );
+}
+
+export default Image;
