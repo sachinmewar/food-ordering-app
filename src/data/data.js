b5ac372daa0b856a427d585cefeef0b926f8c352
unknown <sachinmewar800@gmail.com>
2021-05-27 20:09:03 +0530

    Added data inside data dir

---
+++ src/data/data.js
@@ -0,0 +1,21 @@
+
+export const foodStore = [
+   { img: 'img1', name: 'Kadai Paneer', amount: 300, category: 'veg' },
+   { img: 'img2', name: 'Samosa', amount: 20, category: 'veg' },
+   { img: 'img3', name: 'Noodles', amount: 80, category: 'veg' },
+   { img: 'img4', name: 'Naan', amount: 40, category: 'veg' },
+   { img: 'img5', name: 'Shahi Paneer', amount: 250, category: 'veg' },
+   { img: 'img6', name: 'Chilli Paneer', amount: 300, category: 'veg' },
+   { img: 'img7', name: 'Onion Pakodei', amount: 80, category: 'veg' },
+   { img: 'img8', name: 'Mix Pakodei', amount: 60, category: 'veg' },
+   { img: 'img9', name: 'Aalo Parantha', amount: 40, category: 'veg' },
+   { img: 'img10', name: 'Onion Parantha', amount: 200, category: 'veg' },
+   { img: 'img11', name: 'Gajar kaa halwa', amount: 100, category: 'veg' },
+   { img: 'img12', name: 'Moong kaa halwa', amount: 75, category: 'veg' },
+   { img: 'img13', name: 'Mutter Paneer', amount: 300, category: 'veg' },
+   { img: 'img14', name: 'Rajma Chawal', amount: 150, category: 'veg' },
+   { img: 'img15', name: 'Fried Rice', amount: 120, category: 'veg' },
+   { img: 'img16', name: 'Momos', amount: 80, category: 'veg' },
+];
+
+export default foodStore;
