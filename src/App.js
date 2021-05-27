ef13899b03d2d3e974e9233f9208d9e1a43f6ccf
unknown <sachinmewar800@gmail.com>
2021-05-27 20:09:42 +0530

    Added Image and Food Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navigation from './components/Navigation';
 import Image from './components/Image'
+import Food from './components/Food'
 
 const App = () => {
   return (
     <div>
       <Navigation />
       <Image />
+      <Food />
     </div>
   );
 };
