23547598797726d9b2ef83af79e5dbd1950983f2
unknown <sachinmewar800@gmail.com>
2021-05-27 20:02:46 +0530

    Added className in Food.js

---
+++ src/components/Food.js
@@ -0,0 +1,11 @@
+import FoodList from "./FoodList";
+import './Food.css';
+const Food = () => {
+   return (
+      <div className='food-container'>
+         <FoodList />
+      </div>
+   );
+};
+
+export default Food;
