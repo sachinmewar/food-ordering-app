e6233ff2ac133300e87bbda802cb6b7ec04587b3
unknown <sachinmewar800@gmail.com>
2021-05-28 21:15:09 +0530

    contains jsx code for CartButton.js

---
+++ src/Cart/CartButton.js
@@ -0,0 +1,13 @@
+import './CartButton.css'
+// import { ShoppingCartOutlinedIcon as sco } from 'react-icons/io';
+const Cart = () => {
+   return (
+      <button>
+         <span> Cart </span>
+         <span> </span>
+         <span> 2 </span>
+      </button>
+   );
+};
+
+export default Cart;
