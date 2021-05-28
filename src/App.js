8abe570461f386dbc6d48651eb1cc064331a9730
unknown <sachinmewar800@gmail.com>
2021-05-28 21:19:35 +0530

    Added new component Cart inside it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Navigation from './components/Navigation';
 import Image from './components/Image'
 import Food from './components/Food'
-
+import Cart from './Cart/Cart'
 const App = () => {
   return (
     <div>
+      <Cart />
       <Navigation />
       <Image />
       <Food />
