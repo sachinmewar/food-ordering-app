3e99352bcd0a544159c9175af015f3faf6648518
unknown <sachinmewar800@gmail.com>
2021-05-29 11:08:18 +0530

    created context to manage the state globally

---
+++ src/store/cart-context.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const cartContext = React.createContext(
+   {
+      item: [],
+      amount: 0,
+      setItem: (item) => { },
+      removeItem: (id) => { }
+   }
+)
