4a0a915db93e14094d076d5dd161977aec1e6407
unknown <sachinmewar800@gmail.com>
2021-06-01 23:13:09 +0530

    added cartContext

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -1,13 +1,30 @@
 import './FoodItem.css';
 import Amount from './Amount';
 import FoodImage from './FoodImage';
+import { useContext } from 'react';
+import cartContext from '../store/cart-context';
+
 const FoodItem = (props) => {
+
+   const cartCtx = useContext(cartContext);
+
+   const price = '$${props.price.toFixed(w)}';
+   const addToCartHandler = (amount) => {
+      cartCtx.addItem({
+         id: props.id,
+         name: props.name,
+         amount: amount,
+         price: props.price
+      }
+
+      );
+   }
    return (
       <div className='foodItem-container'>
          <FoodImage imgName={props.imgName} />
          <h2> {props.name} </h2>
-         <div id='amount'> ₹{props.amount} </div>
-         <Amount />
+         <div id='amount'> ₹{props.price} </div>
+         <Amount addToCart={addToCartHandler} />
       </div>
    );
 }
