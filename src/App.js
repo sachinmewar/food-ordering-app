ee55e5f5e8af983c334145cff42dda154b2e5a0b
unknown <sachinmewar800@gmail.com>
2021-06-07 15:44:39 +0530

    Food-order-app will only be shown once authentication is done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Image from './components/Image'
 import Food from './components/Food'
 import Cart from './Cart/Cart';
 import CartProvider from './store/CartProvider';
-
+import Auth from './Authentication/Auth'
+import { BrowserRouter } from 'react-router-dom';
 const App = () => {
   const [isCartVisible, setIsCartVisible] = useState(false);
 
@@ -16,15 +17,29 @@ const App = () => {
     setIsCartVisible(value);
   };
 
+  const [authenticated, setAuthenticated] = useState(false);
+
+  const checkIfAuthenticated = (value) => {
+    setAuthenticated(value)
+  }
   return (
     <CartProvider>
-      <div>
-        {isCartVisible && <Cart onClickClose={onClickCloseButtonHandler} />}
-        <Navigation onCartChange={onCartChangeEventHandler} />
-        <Image />
-        <Food />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Auth authenticated={checkIfAuthenticated} />
+          {
+            authenticated &&
+            <div>
+              {isCartVisible && <Cart onClickClose={onClickCloseButtonHandler} />}
+              <Navigation onCartChange={onCartChangeEventHandler} />
+              <Image />
+              <Food />
+            </div>
+          }
+        </div>
+      </BrowserRouter>
     </CartProvider>
+
   );
 };
 
