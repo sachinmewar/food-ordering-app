cf861e6e17000d49821cdae214ac2950aaaac7cb
unknown <sachinmewar800@gmail.com>
2021-06-08 20:13:07 +0530

    Contains user login info

--- src/Authentication/UserInfo.js
+++ src/Authentication/UserInfo.js
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react';
 
-const UserInfo = () => {
+const UserInfo = ({ keycloak }) => {
+   const [name, setName] = useState('');
+   const [email, setEmail] = useState('');
+   const [id, setId] = useState('');
+
+   useEffect = (
+      keycloak.loadUserInfo().then(
+         userInfo => {
+            setName(userInfo.name);
+            setEmail(userInfo.email);
+            setId(userInfo.id);
+         }
+      ), []);
    return (
-      <div> </div>
-   )
+      <div className="UserInfo">
+         <p>Name: {name}</p>
+         <p>Email: {email}</p>
+         <p>ID: {id}</p>
+      </div>
+   );
 }
+
 export default UserInfo;
