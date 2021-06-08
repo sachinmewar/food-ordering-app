e325dd6ad3d5c00bbd8e4bcc4104ae061acdd3dc
unknown <sachinmewar800@gmail.com>
2021-06-08 20:10:15 +0530

    will log you out

--- src/Authentication/Logout.js
+++ src/Authentication/Logout.js
@@ -2,16 +2,16 @@
 // import keycloak from 'keycloak-js';
 // import { useHistory } from 'react-router-dom';
 
-const Logout = ({ keycloak, logoutHandler }) => {
+const Logout = ({ keycloak }) => {
    // const history = useHistory();
    const onLogoutHandler = () => {
       // history.push('/');
       keycloak.logout();
-      logoutHandler(false);
+      // logoutHandler(false);
    }
 
    return (
-      <button onClick={onLogoutHandler}> Logout </button>
+      <button onClick={onLogoutHandler} className='logout-button'> Logout </button>
    );
 }
 
