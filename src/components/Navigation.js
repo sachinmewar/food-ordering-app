974c2c8771f3cace96bad881479e422315eff7bc
unknown <sachinmewar800@gmail.com>
2021-06-08 12:02:29 +0530

    Will pass props to ProfileButton

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,25 @@
+import { useState } from 'react';
 import './Navigation.css';
 import CartButton from '../Cart/CartButton';
-import Profile from './Profile';
+import ProfileButton from './UserProfile/ProfileButton';
 
 const Navigation = (props) => {
 
    const onButtonClickEventHandler = (value) => {
       props.onCartChange(value);
    }
+
+   const onProfileButtonClickHandler = (value) => {
+      props.onProfileClick(value);
+   }
+
    return (
       <header className='navigation-container'>
          <p id='logo'> Logo </p>
          <p id='location'> Location </p>
          <input type='search' id='search-bar' />
          <CartButton className='cart' onButtonClick={onButtonClickEventHandler} />
-         <Profile />
+         <ProfileButton onProfileClick={onProfileButtonClickHandler} />
       </header>
    );
 };
