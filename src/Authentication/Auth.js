a0216c4ba79aa0fa63d645fa00d84634146c35c2
unknown <sachinmewar800@gmail.com>
2021-06-08 20:09:53 +0530

    will return null

--- src/Authentication/Auth.js
+++ src/Authentication/Auth.js
@@ -1,8 +1,6 @@
 
 import { useState, useEffect } from 'react';
 import Keycloak from 'keycloak-js';
-import UserInfo from './UserInfo';
-import Logout from './Logout';
 
 const Auth = (props) => {
 
@@ -18,22 +16,11 @@ const Auth = (props) => {
          authenticated => {
             setKeycloakValue(keycloak);
             setIsAuthenticated(authenticated);
-            props.authenticated(authenticated);
          }
       )
    }, [])
-
-   props.authenticated(IsAuthenticated);
-
-   return (
-      <div>
-         {
-            IsAuthenticated &&
-            <UserInfo keycloak={keycloakValue} /> &&
-            <Logout keycloak={keycloakValue} logout={props.authenticated} />
-         }
-      </div>
-   )
+   props.keycloakValues(IsAuthenticated, keycloakValue)
+   return null;
 }
 
 export default Auth;
