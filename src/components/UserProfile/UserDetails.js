ae8fcf59cd086caed6ec535cd3d3c059037e83c3
unknown <sachinmewar800@gmail.com>
2021-06-08 12:00:12 +0530

    Contains hard coded details of user

---
+++ src/components/UserProfile/UserDetails.js
@@ -0,0 +1,12 @@
+import './UserDetails.css';
+const UserDetails = () => {
+   return (
+      <div className='user-details'>
+         <div> Image </div>
+         <div id='name'> Sachin Mewar </div>
+         <div id='email'> test@test.com </div>
+      </div>
+   )
+}
+
+export default UserDetails;
