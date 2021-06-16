f88b2dfdb9362951d495c8dc0e85b13fe2eb0f1e
unknown <sachinmewar800@gmail.com>
2021-06-16 16:04:09 +0530

    Changes made in CartItem.js

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,16 +1,29 @@
 import './CartItem.css';
-
 const CartItem = (props) => {
 
    return (
       <div className='cart-item__container'>
          <h2> {props.name} </h2>
          <div id='price'> ₹{props.price} </div>
-                  <button onClick={props.onItemRemove}> - </button>
+         <button onClick={props.onItemRemove}> - </button>
          <div id='amount'> {props.amount} </div>
          <button onClick={props.onItemAdd}> + </button>
       </div>
+      // <li className={classes['cart-item']}>
+      //    <div>
+      //       <h2>{props.name}</h2>
+      //       <div className={classes.summary}>
+      //          <span className={classes.price}>{props.price}</span>
+      //          <span className={classes.amount}>x {props.amount}</span>
+      //       </div>
+      //    </div>
+      //    <div className={classes.actions}>
+      //       <button onClick={props.onItemRemove}>−</button>
+      //       <button onClick={props.onItemAdd}>+</button>
+      //    </div>
+      // </li>
    );
 }
 
 export default CartItem;
+
